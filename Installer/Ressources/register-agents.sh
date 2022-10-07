923bda2491aa1deadd265703a9f8dbf8b3627d1c
Adélaïde Sky <augustin.humez@gmail.com>
2022-10-07 23:54:08 +0200

    🐛 Gonna fix this bug one day

--- Installer/Ressources/register-agents.sh
+++ Installer/Ressources/register-agents.sh
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fdce67452bfcbe59151d2574d39470f36d877257b33f9f2d3473521470c037d8
-size 85
+#!/bin/bash
+#welp
+sudo launchctl load /Library/LaunchAgents/com.adesky.goxlr.plist
+
+exit 0
