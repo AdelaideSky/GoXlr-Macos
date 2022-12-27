eae21dd1ffc6713721d21cedebfdec7c8b380da2
Adélaïde Sky <augustin.humez@gmail.com>
2022-12-27 14:14:53 +0100

    Initial Commit

---
+++ GoXlr-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoXlr-App
+//
+//  Created by Adélaïde Sky on 27/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoXlr-App/GoXlr_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoXlr_AppApp.swift
+//  GoXlr-App
+//
+//  Created by Adélaïde Sky on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoXlr_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
